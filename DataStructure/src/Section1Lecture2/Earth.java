d145163e0d01e8c8605636ca073823a9e110ee89
himanshu.patwardhan <patwardhan.himanshu@gmail.com>
2017-08-20 00:35:33 +0530

    Master Commit

--- DataStructure/src/Section1Lecture2/Earth.java
+++ DataStructure/src/Section1Lecture2/Earth.java
@@ -4,4 +4,17 @@ package Section1Lecture2;
  * Created by himanshu.patwardhan on 19-Aug-17.
  */
 public class Earth {
+
+    public static void main(String[] args) {
+
+        Human jack=new Human();
+        jack.setName("Jack");
+        jack.setAge(24);
+        jack.setHeight(180);
+        jack.setEyeColor("Blue");
+
+        jack.speak();
+        jack.eat();
+        jack.walk();
+    }
 }
--- DataStructure/src/Section1Lecture2/Human.java
+++ DataStructure/src/Section1Lecture2/Human.java
@@ -4,4 +4,68 @@ package Section1Lecture2;
  * Created by himanshu.patwardhan on 19-Aug-17.
  */
 public class Human {
+
+    private String name;
+    private int age;
+    private int height;
+    private String eyeColor;
+
+    public Human() {
+    }
+
+    public Human(String name, int age, int height, String eyeColor) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.eyeColor = eyeColor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public void speak(){
+        System.out.println("Hello, My name is "+this.getName());
+        System.out.println("My age is "+this.getAge());
+        System.out.println("My height is "+this.getHeight()+" cms");
+        System.out.println("My eye color is "+this.getEyeColor());
+    }
+
+    public void walk(){
+
+        System.out.println("Lets go for a walk !!");
+    }
+
+    public void eat(){
+
+        System.out.println("Can we go out to eat something ?");
+    }
 }
